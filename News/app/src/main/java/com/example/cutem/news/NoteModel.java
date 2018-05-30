0f05592fb6e8e892de305e677118f1d0d1acc09e
chioreandan <chiorean.dan12@gmail.com>
2018-05-31 02:58:56 +0300

    Add NoteModel

---
+++ News/app/src/main/java/com/example/cutem/news/NoteModel.java
@@ -0,0 +1,30 @@
+package com.example.cutem.news;
+
+public class NoteModel {
+    public String noteTitle;
+    public String noteTime;
+    public NoteModel(){
+
+    }
+
+    public NoteModel(String noteTitle, String noteTime) {
+        this.noteTitle = noteTitle;
+        this.noteTime = noteTime;
+    }
+
+    public String getNoteTitle() {
+        return noteTitle;
+    }
+
+    public String getNoteTime() {
+        return noteTime;
+    }
+
+    public void setNoteTitle(String noteTitle) {
+        this.noteTitle = noteTitle;
+    }
+
+    public void setNoteTime(String noteTime) {
+        this.noteTime = noteTime;
+    }
+}
